cc8d0949542c4d0f484c42e1656a56bbfefcfe2c
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-11-08 01:35:53 +1300

    add card components

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,12 @@ module.exports = {
         DarkViolet: "hsl(257, 27%, 26%)",
         Red: "hsl(0, 87%, 67%)",
         Gray: "hsl(0, 0%, 75%)",
+        LightViolet: "hsl(255,33%,95%)",
         GrayishViolet: "hsl(257, 7%, 63%)",
         VeryDarkBlue: "hsl(255, 11%, 22%)",
         VeryDarkViolet: "hsl(260, 8%, 14%)",
       },
-    },
+     },
   },
   plugins: [],
 };
