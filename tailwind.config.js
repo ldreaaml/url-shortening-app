1aadb8bc53fcec05a738bf1877ac0bf5b3f00b1c
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-11-07 20:46:58 +1300

    set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        Cyan: "hsl(180, 66%, 49%)",
+        DarkViolet: "hsl(257, 27%, 26%)",
+        Red: "hsl(0, 87%, 67%)",
+        Gray: "hsl(0, 0%, 75%)",
+        GrayishViolet: "hsl(257, 7%, 63%)",
+        VeryDarkBlue: "hsl(255, 11%, 22%)",
+        VeryDarkViolet: "hsl(260, 8%, 14%)",
+      },
+    },
+  },
+  plugins: [],
+};
