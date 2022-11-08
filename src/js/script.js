4403a9ddff63b9488fea2dcac3ec6ce18c69f723
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-11-09 01:04:23 +1300

    make api call to get shortened link

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,63 @@
-const btn = document.getElementById("menu-btn");
+const navBtn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
+const shortenBtn = document.getElementById("shorten-btn");
+const error = document.getElementById("error");
+const form = document.getElementById("form");
+const input = document.getElementById("txtBox");
+const result = document.getElementById("result");
+const copyBtn = document.getElementById("copyBtn");
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
+navBtn.addEventListener("click", () => {
+  navBtn.classList.toggle("open");
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
 });
+
+input.addEventListener("input", () => {
+  error.classList.add("hidden");
+  input.classList.remove("txtBoxWarning");
+});
+
+form.addEventListener("submit", () => {
+  if (input.value.match("^$")) {
+    error.classList.remove("hidden");
+    input.classList.add("txtBoxWarning");
+  } else {
+    // make api call
+    const url = input.value;
+    console.log(">" + url);
+    getShortenedLink(url).then((data) => {
+      document.getElementById("original-link").innerText = url;
+      document.getElementById("short-link").innerText = data.result.short_link;
+    });
+  }
+});
+
+//copy to clipboard
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(copyBtn.previousElementSibling.innerHTML);
+});
+
+async function getShortenedLink(link) {
+  const url = "https://api.shrtco.de/v2/shorten?url=" + link;
+  console.log(url);
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
